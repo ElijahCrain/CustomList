39c0e8c99c1976e3178ca8cac89a73185d548e09
ElijahCrane <chris.maychet.t1@gmail.com>
2021-02-23 15:18:03 -0600

    added a remove method test remove item count decreases by one .

--- CustomListTest/RemoveMethodUnitTest.cs
+++ CustomListTest/RemoveMethodUnitTest.cs
@@ -8,8 +8,48 @@ namespace CustomListTest
 	class RemoveMethodUnitTest
 	{
 		[TestMethod]
-		public void remove()
+		public void remove_Item_CountDecreases_ByOne()
 		{
+			//Arange
+
+			CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+			int number1 = 9;
+			int number2 = 7;
+			int expected = 1;
+			int actual;
+
+			//Act
+			collectionOfNumbers.Add(number1);
+			collectionOfNumbers.Add(number2);
+			collectionOfNumbers.Remove(number1);
+			actual = collectionOfNumbers.Count;
+
+			//Assert
+
+			Assert.AreEqual(expected, actual);
+		}
+		
+		public void Test_2()
+		{
+			//Arange
+			CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+			//Act
+
+			//Assert
+
+
+		}
+		public void Test_3()
+		{
+			//Arange
+			CustomList<int> collectionOfNumbers = new CustomList<int>();
+
+			//Act
+
+			//Assert
+
 
 		}
 	} 
